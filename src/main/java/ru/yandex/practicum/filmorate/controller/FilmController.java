3f52ef956b45dab1e6e224b2fc8f5b4542dddedd
TireNik <sugarman.sviridov@yandex.ru>
2025-01-02 18:48:39 +0300

    Уменьшил количество запросов к базе (кажется уменьшил)

--- src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
+++ src/main/java/ru/yandex/practicum/filmorate/controller/FilmController.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.filmorate.controller;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import ru.yandex.practicum.filmorate.model.Film;
 import ru.yandex.practicum.filmorate.service.FilmService;
@@ -13,7 +12,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/films")
-@Validated
+
 @Slf4j
 @RequiredArgsConstructor
 public class FilmController {
