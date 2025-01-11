03394ecdc74527ca6ea655feefa75d9e0742fc8e
vBoltunov <vitaliy.boltunov@yandex.ru>
2025-01-11 20:35:04 +0300

    Метод getPopularFilms(): внёс ещё исправления, чтобы не падал тест Postman

--- src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/dao/FilmDbStorage.java
@@ -294,42 +294,6 @@ public class FilmDbStorage implements FilmStorage {
         return film;
     }
 
-    /*@Override
-    public List<Film> getPopularFilms(int count, Integer genreId, Integer year) {
-        String insertion = "";
-        if (genreId == null && year != null) {
-            insertion = "WHERE YEAR(f.releaseDate)=" + year;
-        }
-        if (genreId != null && year == null) {
-            insertion = "LEFT JOIN FILM_GENRES AS fg ON fg.FILM_ID = f.FILM_ID WHERE fg.GENRE_ID =" + genreId;
-        }
-        if (genreId != null && year != null) {
-            insertion = "LEFT JOIN FILM_GENRES AS fg ON fg.FILM_ID = f.FILM_ID WHERE fg.GENRE_ID =" + genreId +
-                    " AND YEAR(f.releaseDate)=" + year;
-        }
-
-        String sql = "SELECT f.film_id, f.name, f.description, f.releaseDate, f.duration, r.rating_id, " +
-                "r.name AS rating_name, COUNT(l.user_id) AS likes " +
-                "FROM films AS f " +
-                "LEFT JOIN likes AS l ON f.film_id = l.film_id " +
-                "LEFT JOIN mpa_rating AS r ON f.rating_id = r.rating_id " +
-                insertion +
-                "GROUP BY f.film_id, r.rating_id, r.name " +
-                "ORDER BY likes DESC " +
-                "LIMIT ?";
-        List<Film> films = jdbc.query(sql, (rs, rowNum) -> filmMapper.mapToFilm(rs), count);
-
-        if (films.isEmpty()) {
-            return films;
-        }
-
-        List<Long> filmsIds = films.stream()
-                .map(Film::getId)
-                .collect(Collectors.toList());
-        addGenresToFilms(filmsIds, films);
-        return films;
-    }*/
-
     @Override
     public List<Film> getPopularFilms(int count, Integer genreId, Integer year) {
         String insertion = "";
@@ -356,15 +320,13 @@ public class FilmDbStorage implements FilmStorage {
         List<Film> films = jdbc.query(sql, (rs, rowNum) -> filmMapper.mapToFilm(rs), count);
 
         if (films.isEmpty()) {
-            log.warn("No popular films found for provided criteria: genreId={}, year={}", genreId, year);
+            return films;
         }
 
-        if (!films.isEmpty()) {
-            List<Long> filmsIds = films.stream()
-                    .map(Film::getId)
-                    .collect(Collectors.toList());
-            addGenresToFilms(filmsIds, films);
-        }
+        List<Long> filmsIds = films.stream()
+                .map(Film::getId)
+                .collect(Collectors.toList());
+        addGenresToFilms(filmsIds, films);
 
         return films;
     }
